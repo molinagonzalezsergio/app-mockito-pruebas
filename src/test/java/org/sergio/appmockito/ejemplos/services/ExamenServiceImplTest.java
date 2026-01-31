ae9661c0f46be755ff5ea26db9b0adab2d768bb9
Sergio Molina <molinagonzalezsergio@gmail.com>
2026-01-31 19:50:22 +0000

    ajustes test examen

--- src/test/java/org/sergio/appmockito/ejemplos/services/ExamenServiceImplTest.java
+++ src/test/java/org/sergio/appmockito/ejemplos/services/ExamenServiceImplTest.java
@@ -3,28 +3,41 @@ package org.sergio.appmockito.ejemplos.services;
 import org.junit.jupiter.api.*;
 
 import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.sergio.appmockito.ejemplos.models.Examen;
 import org.sergio.appmockito.ejemplos.repositories.ExamenRepository;
 import org.sergio.appmockito.ejemplos.repositories.PreguntaRepository;
-
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
+@ExtendWith(MockitoExtension.class)
 class ExamenServiceImplTest {
-    private ExamenRepository repository;
-    private ExamenService service;
+
+    @Mock
+    ExamenRepository repository;
+
+    @Mock
     PreguntaRepository preguntaRepository;
+
+    @InjectMocks
+    ExamenServiceImpl service;
 
     @BeforeEach
     void setUp() {
-        this.repository= mock(ExamenRepository.class);
-        preguntaRepository=mock(PreguntaRepository.class);
-        this.service=new ExamenServiceImpl(repository,preguntaRepository);
+//        MockitoAnnotations.openMocks(this);
+//        Usando las anotaciones de MOCKITO, sería lo mismo, que lo comentadol..
+//        this.repository= mock(ExamenRepository.class);
+//        this.preguntaRepository=mock(PreguntaRepository.class);
+//        this.service=new ExamenServiceImpl(repository,preguntaRepository);
+
     }
 
     @Tag("Buscar_Examen")
@@ -52,7 +65,7 @@ class ExamenServiceImplTest {
         }
 
         @Test
-        @DisplayName("Prueba Obtener Lista de Preguntas Existentes")
+        @DisplayName("Prueba Obtener Lista de Preguntas Existentes Para Matemáticas")
         void testPreguntasExamenMatematicas() {
             when(repository.findAll()).thenReturn(Datos.EXAMENES);
             when(preguntaRepository.findPreguntasPorExamenId(5L)).thenReturn(Datos.PREGUNTAS);
@@ -61,7 +74,7 @@ class ExamenServiceImplTest {
         }
 
         @Test
-        @DisplayName("Prueba Obtener Lista de Preguntas")
+        @DisplayName("Prueba Obtener Lista de Preguntas Para Historia")
         void testPreguntasExamenHistoria() {
             when(repository.findAll()).thenReturn(Datos.EXAMENES);
             when(preguntaRepository.findPreguntasPorExamenId(7L)).thenReturn(Datos.PREGUNTAS);
@@ -70,13 +83,38 @@ class ExamenServiceImplTest {
         }
 
         @Test
-        @DisplayName("Prueba Obtener Lista de Preguntas")
+        @DisplayName("Prueba Obtener Lista de Preguntas Para Cualquiera")
         void testPreguntasExamenAny() {
             when(repository.findAll()).thenReturn(Datos.EXAMENES);
             when(preguntaRepository.findPreguntasPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS);
             Examen examen=service.findExamenPorNombreConPreguntas("Historia");
-            assertTrue(examen.getPreguntas().contains("Aritméticas"));
+            assertTrue(examen.getPreguntas().contains("Integrales"));
         }
+
+        @Test
+        @DisplayName("Prueba Verificar Lista de Preguntas")
+        void testPreguntasExamenVerify() {
+            when(repository.findAll()).thenReturn(Datos.EXAMENES);
+            when(preguntaRepository.findPreguntasPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS);
+            Examen examen=service.findExamenPorNombreConPreguntas("Matemáticas");
+            assertEquals(5,examen.getPreguntas().size());
+            assertTrue(examen.getPreguntas().contains("Trigonometrías"));
+            verify(repository).findAll();
+            verify(preguntaRepository).findPreguntasPorExamenId(5L);
+        }
+
+        @Test
+        @DisplayName("Prueba No existe Lista de Preguntas")
+        void testNoExisteExamenVerify() {
+            when(repository.findAll()).thenReturn(Datos.EXAMENES);
+            when(preguntaRepository.findPreguntasPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS);
+            Examen examen=service.findExamenPorNombreConPreguntas("Matemáticas");
+            assertTrue(examen.getPreguntas().contains("Trigonometrías"));
+            verify(repository).findAll();
+            verify(preguntaRepository).findPreguntasPorExamenId(5L);
+        }
+
+
     }
 
 }
